11d32f10124131705123b204605986daa4c8968c
Flavius <flaviuspopoiu@gmail.com>
2019-07-27 18:25:04 +0300

    first commit with Navigation

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import Header from './components/navigation/Header'
+
+export default {
+  name: 'App',
+  components: {
+    'app-header': Header
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/navigation/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Stock Trader</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Stocks</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav navbar-right mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">End Day</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Save & Load
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Save</a>
+            <a class="dropdown-item" href="#">Load</a>
+          </div>
+        </li>
+      </ul>
+      <div class="nav nav-item">
+        <span class="navbar-text">Funds: $10000</span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<style scoped>
+
+</style>
